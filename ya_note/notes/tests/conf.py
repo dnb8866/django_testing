070a34ed7a72c5efc3e61a946ee71bc0fb9e5360
Denis Ivanov <dnb8866@gmail.com>
2024-10-19 21:07:55 +0700

    for review, iteration 5

--- ya_note/notes/tests/conf.py
+++ ya_note/notes/tests/conf.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.test import Client, TestCase
 from django.urls import reverse
-from pytils.translit import slugify
 
 from notes.models import Note
 
